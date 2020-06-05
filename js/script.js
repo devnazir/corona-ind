753a8666a4fe3275225128f7d3ceda9181a0c174
devnazir <nazirbachrululum@gmail.com>
2020-06-05 20:03:45 +0700

    menghapus variabel yang valuenya dipanggil secara berulang

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-const jumlahPositif = document.querySelector('.positif');
-const jumlahMeninggal = document.querySelector('.meninggal');
-const jumlahSembuh = document.querySelector('.sembuh');
 window.addEventListener('load', async function () {
-    jumlahPositif.innerHTML = await PositifCorona;
-    jumlahMeninggal.innerHTML = await Meninggal;
-    jumlahSembuh.innerHTML = await Sembuh;
+    const data = await dataCoronaIndonesia();
+    updateUI(data);
 });
 
-const PositifCorona = dataCoronaIndonesia()
-    .then(response => {
-        return response.confirmed.value;
-    });
+function updateUI(data) {
+    const jumlahPositif = document.querySelector('.positif');
+    const jumlahMeninggal = document.querySelector('.meninggal');
+    const jumlahSembuh = document.querySelector('.sembuh');
 
-const Meninggal = dataCoronaIndonesia()
-    .then(response => {
-        return response.deaths.value;
-    });
-
-const Sembuh = dataCoronaIndonesia()
-    .then(response => {
-        return response.recovered.value;
-    });
+    jumlahPositif.innerHTML = data.confirmed.value;
+    jumlahMeninggal.innerHTML = data.deaths.value;
+    jumlahSembuh.innerHTML = data.recovered.value;
+}
 
 
 async function dataCoronaIndonesia() {
